b0c5639e2109c5dfb7dc24e58fdc39a11d3e0d37
Teodor Plop <teodorplop@yahoo.com>
2018-03-25 14:04:33 +0300

    Integrated DoTween

---
+++ Assets/Scripts/Interface/Panels/UIMenuPanel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIMenuPanel : MonoBehaviour {
+	protected bool shown;
+
+	public virtual void Show() {
+		gameObject.SetActive(shown = true);
+	}
+
+	public virtual void Hide() {
+		gameObject.SetActive(shown = false);
+	}
+
+	public void Toggle() {
+		if (shown) Hide();
+		else Show();
+	}
+}
---
+++ Assets/Scripts/Interface/Panels/UITweenMenuPanel.cs
@@ -0,0 +1,21 @@
+﻿using DG.Tweening;
+
+public class UITweenMenuPanel : UIMenuPanel {
+	private DOTweenAnimation[] tweens;
+
+	void OnEnable() {
+		tweens = GetComponents<DOTweenAnimation>();
+	}
+
+	public override void Show() {
+		shown = true;
+		foreach (DOTweenAnimation tween in tweens)
+			tween.DOPlayForward();
+	}
+
+	public override void Hide() {
+		shown = false;
+		foreach (DOTweenAnimation tween in tweens)
+			tween.DOPlayBackwards();
+	}
+}
